1ed0497efac6074f35c39b41ff32f5acce50e754
drdynscript <drdynscript@gmail.com>
2020-05-23 20:28:27 +0200

    Added design in the app

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -4,7 +4,37 @@ class Footer {
   async render () {
     return `
       <footer class="footer">
-        FOOTER      
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+              <section class="footer-section">
+                <h1 class="footer-section__title">gdm.gent</h1>
+                <ul class="footer-section__list">
+                  <li class="footer-section__item"><a href="${routes.HOME}" data-navigo>Home</a></li>
+                  <li class="footer-section__item"><a href="${routes.ABOUT}" data-navigo>About</a></li>
+                  <li class="footer-section__item"><a href="${routes.BLOG}" data-navigo>Blog</a></li>
+                  <li class="footer-section__item"><a href="${routes.CASES}" data-navigo>Cases</a></li>
+                  <li class="footer-section__item"><a href="${routes.TEAM}" data-navigo>Team</a></li> 
+                  <li class="footer-section__item"><a href="${routes.CONTACT}" data-navigo>Contact</a></li>
+                </ul>
+              </section>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <section class="footer-section">
+                <h1 class="hidden footer-section__title">Social media</h1>
+              </section>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <section class="footer-section">
+                <h1 class="hidden footer-section__title">Terms</h1>
+              </section>
+            </div>
+          </div>
+        </div>      
       </footer>
     `;
   }
